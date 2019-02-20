1445f6e890699b102b12e1217e144a3963d98f2d
SCArthi <arthika19@gmail.com>
2019-02-20 18:01:41 +0530

    Updated configurations

--- src/backend/DBConnect.java
+++ src/backend/DBConnect.java
@@ -26,12 +26,13 @@ public class DBConnect {
        
       */
 
+       Read r = new Read();
        try{
-            InetAddress addr = InetAddress.getByName("localhost"); //DESKTOP-K4EFD5D
+            InetAddress addr = InetAddress.getByName(r.getProperty("host")); //DESKTOP-K4EFD5D
             //DESKTOP-K4EFD5D
             String host = addr.getHostAddress();
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://"+host+"/prema?zeroDateTimeBehavior=convertToNull","root","123");
+            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://"+host+"/prema?zeroDateTimeBehavior=convertToNull",r.getProperty("username"),r.getProperty("password"));
             return con;  
        }catch(Exception e){
             JOptionPane.showMessageDialog(null, e);
